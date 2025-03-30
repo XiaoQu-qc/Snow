fe000354d5b361f7aef2abf01fcce5e22b4c36da
XiaoQu-qc <931886861@qq.com>
2025-03-30 19:08:57 +0800

    node

--- internal/plumtree/core.go
+++ internal/plumtree/core.go
@@ -1,6 +1,7 @@
 package plumtree
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	. "snow/common"
@@ -69,6 +70,23 @@ func (s *Server) Hand(msg []byte, conn net.Conn) {
 			msgId := msg[TagLen+IpLen : IpLen+TagLen+TimeLen]
 			s.msgCache.Set(string(msgId), string(msg), s.Config.ExpirationTime)
 			s.MessageIdQueue <- msgId
+			ipByte := msg[TagLen : TagLen+IpLen]
+			switch msgAction {
+			case NodeJoin:
+				s.Member.AddMember(ipByte, NodeSurvival)
+
+				if !bytes.Equal(ipByte, s.Config.IPBytes()) {
+					s.eagerLock.Lock()
+					s.EagerPush = append(s.EagerPush, parentIP)
+					s.eagerLock.Unlock()
+				}
+
+			case NodeLeave:
+				s.Member.RemoveMember(ipByte, false)
+				s.eagerLock.Lock()
+				s.EagerPush = removeString(s.EagerPush, parentIP)
+				s.eagerLock.Unlock()
+			}
 			//对消息进行转发
 			s.PlumTreeMessage(msg)
 		}
